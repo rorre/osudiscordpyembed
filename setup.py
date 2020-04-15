d77e4284b7eda6ddd5e14d88ee6e87fe8474fee3
Kyuunex <kyuunex@protonmail.ch>
2020-04-15 16:14:04 +0400

    Semantic versioning

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='osuwebembed',
     packages=['osuwebembed'],
-    version='r20191125',
+    version='2.0.0-array',
     description='osuwebembed',
     author='Kyuunex',
     url='https://github.com/Kyuunex/osudiscordpyembed',
-    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/r20191125',
+    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/2.0.0-array',
     keywords=['osu', 'discord'],
     classifiers=[],
     requires=['discord.py'],
